cc63cd2478a3de3f173709459673a9d927f33057
Soheil <soheilrajabali@gmail.com>
2023-04-27 21:54:53 -0400

    feat: Goomba Drivetrain Code

---
+++ TeamCode/src/main/java/org/firstinspires/Goomba.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp
+public class Goomba extends LinearOpMode {
+
+    DcMotor motorFL, motorBL, motorFR, motorBR;
+
+    @Override
+    public void runOpMode() {
+        // Movement Motors
+        DcMotor motorFL = hardwareMap.get(DcMotor.class, "motorFrontLeft");
+        DcMotor motorBL = hardwareMap.get(DcMotor.class, "motorBackLeft");
+        DcMotor motorFR = hardwareMap.get(DcMotor.class, "motorFrontRight");
+        DcMotor motorBR = hardwareMap.get(DcMotor.class, "motorBackRight");
+
+        //Reverse left side motors
+        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+            // forward, back
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+
+            // strafing
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+
+            // turning
+            double rx = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            motorFL.setPower(frontLeftPower);
+            motorBL.setPower(backLeftPower);
+            motorFR.setPower(frontRightPower);
+            motorBR.setPower(backRightPower);
+        }
+    }
+}
